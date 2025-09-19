b89342fc42c9d17b2211d07b7034aabe1c22815d
Shreyas <sovanishreyas@gmail.com>
2025-09-19 19:18:59 +0000

    delete unnecessary comments

--- ignition/modules/FundMe.js
+++ ignition/modules/FundMe.js
@@ -25,11 +25,3 @@ module.exports = buildModule("FundMeModuleV2", (m) => {//Used V2 to avoid cachin
     
 });
 
-// # For localhost - first deploy the mock, then deploy FundMe with mock address
-// npx hardhat ignition deploy ignition/modules/MockV3Aggregator.js --network localhost
-
-// # After getting the mock address from above deployment, use it:
-// npx hardhat ignition deploy ignition/modules/FundMe.js --network localhost --parameters '{"FundMeModule": {"ethUsdPriceFeed": "MOCK_ADDRESS_FROM_ABOVE"}}'
-
-// # For Sepolia
-// npx hardhat ignition deploy ignition/modules/FundMe.js --network sepolia --parameters '{"FundMeModule": {"ethUsdPriceFeed": "0x694AA1769357215DE4FAC081bf1f309aDC325306"}}'
